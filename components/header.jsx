b800bde983539f4e9f46eaca70787869dc680771
Karan Mundre <karanmundre73@gmail.com>
2025-05-20 14:28:32 +0530

    Basic changes

--- components/header.jsx
+++ components/header.jsx
@@ -23,18 +23,19 @@ export default async function Header() {
   await checkUser();
 
   return (
-    <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
-      <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+    <header className="fixed top-0 w-full border-b bg-background/80  backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
+      <nav className="container mx-auto px-4 h-16 flex items-center justify-between bg-slate-900/80">
+        {" "}
         <Link href="/">
           <Image
             src={"/logo.png"}
             alt="Sensai Logo"
             width={200}
             height={60}
-            className="h-12 py-1 w-auto object-contain"
+            className="h-20 w-auto object-contain"
+            priority
           />
         </Link>
-
         {/* Action Buttons */}
         <div className="flex items-center space-x-2 md:space-x-4">
           <SignedIn>
